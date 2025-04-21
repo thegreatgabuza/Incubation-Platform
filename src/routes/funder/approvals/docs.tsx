22cab0689c8d88b75897f59be4f6de9a779c6827
Daniel-Rumona <danielrumona@gmail.com>
2025-04-22 00:08:23 +0200

    Update docs.tsx

--- src/routes/funder/approvals/docs.tsx
+++ src/routes/funder/approvals/docs.tsx
@@ -1,1 +1,20 @@
-gg
+// ApprovalQueue.tsx
+import React from "react";
+import { Form, Input, Button, Select } from "antd";
+
+export const ApprovalQueue = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values: any) => {
+    console.log("Approval Decision:", values);
+  };
+
+  return (
+    <Form layout="vertical" form={form} onFinish={onFinish}>
+      <Form.Item name="proposal" label="Proposal ID" rules={[{ required: true }]}> <Input /> </Form.Item>
+      <Form.Item name="decision" label="Decision" rules={[{ required: true }]}> <Select options={[{ value: 'approve', label: 'Approve' }, { value: 'reject', label: 'Reject' }]} /> </Form.Item>
+      <Form.Item name="comments" label="Comments"> <Input.TextArea rows={3} /> </Form.Item>
+      <Form.Item> <Button type="primary" htmlType="submit">Submit Decision</Button> </Form.Item>
+    </Form>
+  );
+};
